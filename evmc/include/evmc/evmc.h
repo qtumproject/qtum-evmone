8e28c9a2cd8f83eb55165df56bcacecb453fff9b
timemarkovqtum <time.markov@qtum.com>
2026-08-05 12:50:21 +0200

    Port create with value evmc status code

--- evmc/include/evmc/evmc.h
+++ evmc/include/evmc/evmc.h
@@ -361,6 +361,9 @@ enum evmc_status_code
     /** The caller does not have enough funds for value transfer. */
     EVMC_INSUFFICIENT_BALANCE = 17,
 
+    /** Qtum specific: create with value is not allowed. */
+    EVMC_CREATE_WITH_VALUE = 1000,
+
     /** EVM implementation generic internal error. */
     EVMC_INTERNAL_ERROR = -1,
 
--- evmc/include/evmc/helpers.h
+++ evmc/include/evmc/helpers.h
@@ -209,6 +209,8 @@ static inline const char* evmc_status_code_to_string(enum evmc_status_code statu
         return "wasm trap";
     case EVMC_INSUFFICIENT_BALANCE:
         return "insufficient balance";
+    case EVMC_CREATE_WITH_VALUE:
+        return "create with value";
     case EVMC_INTERNAL_ERROR:
         return "internal error";
     case EVMC_REJECTED:
